dcf826f407bd50560de655d03b4bcd1f03668e8e
Yetsy <73970022+Yetsy@users.noreply.github.com>
2025-09-05 10:43:58 -0500

    AVANCE

---
+++ src/Snoopy.java
@@ -0,0 +1,14 @@
+public class Snoopy {
+    public static void main(String[] args) {
+        System.out.println("   ▄▄▄▄▄▄▄");
+        System.out.println("  █ ▄▄▄▄▄ █");
+        System.out.println("  █ █   █ █");
+        System.out.println("  █ █▄▄▄█ █");
+        System.out.println("  █▄▄▄▄▄▄▄█");
+        System.out.println("     ▄▄▄ ");
+        System.out.println("   ( o_o )   Snoopy says: Hello from IntelliJ!");
+        System.out.println("   /)   (\\");
+        System.out.println("  (   .   )");
+        System.out.println("   `-...-' ");
+    }
+}
